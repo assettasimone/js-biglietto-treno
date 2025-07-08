c39800768c8662f476e7a58f897c7ecc82a10aa5
assettasimone <assettasimone@gmail.com>
2025-07-08 16:05:05 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+/* Ask Tickets km */
+const ticketKm = prompt('Inserisci il numero di kilometri per il biglietto');
+console.log(ticketKm);
+
+/* Ask user Age */
+const userAge = prompt('Inserisci la tua età');
+console.log(userAge);
