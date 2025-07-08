5ee6d551f66654d5e72a3d1a8e9b30e76fbf02b5
assettasimone <assettasimone@gmail.com>
2025-07-08 16:19:41 +0200

    add discount

--- main.js
+++ main.js
@@ -5,3 +5,24 @@ console.log(ticketKm);
 /* Ask user Age */
 const userAge = prompt('Inserisci la tua età');
 console.log(userAge);
+
+/* Calcolo il prezzo del biglietto base in base ai km */
+
+const basePrice = ticketKm * 0.21;
+
+/* Controllo l'età dell'utente per applicare scontistiche */
+let finalPrice = 1;
+
+
+if (userAge > 65) {
+    /* applico lo sconto del 40% al prezzo di base */
+    finalPrice = basePrice - (basePrice / 100 * 40)
+    console.log(finalPrice)
+} else if (userAge < 18) {
+    /* applico lo sconto del 40% al prezzo di base */
+    finalPrice = basePrice - (basePrice / 100 * 20)
+    console.log('lo sconto del 20% è stato applicato' + finalPrice)
+}
+else {
+    console.log('Non puoi ricevere uno sconto')
+}
